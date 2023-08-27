a6ed536575fe63153502fd8d45a937650b742302
Dmitriy Silaev <s91097221699@gmail.com>
2023-08-27 18:41:23 +0300

    My first commit in_kits yandex_api_stand_tests

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://383f33a3-b5e2-49a4-8c85-65c85e7aa606.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
